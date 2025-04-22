5bbd1bcb2692b141dd226933f4ce4d8ef9c02e89
Alistair Peat <alistair.peat@gmail.com>
2025-04-22 07:58:12 +0100

    test

--- timer.js
+++ timer.js
@@ -2,8 +2,7 @@ let udoi = localStorage.dateOfInterest;
 let eoi = localStorage.eventOfInterest;
 
 
-if(!udoi){
-    
+if(!udoi){    
     /*if retreived udoi is empty, ask the user for an event and date, 
     and then assign these to localStorage variables*/
     eoi = prompt(`Enter the event: `, `Tea Time`);
@@ -13,69 +12,62 @@ if(!udoi){
     localStorage.eventOfInterest = eoi;
 }
 
+let eventTitleElement = document.getElementById("timer-title");
 let daysElement = document.getElementById("days");
 let hoursElement = document.getElementById("hours");
 let minutesElement = document.getElementById("minutes");
 let secondsElement = document.getElementById("seconds");
-    
-let eventTitleElement = document.getElementById("timer-title");
+
 eventTitleElement.innerHTML = "<h1>"+ eoi + " on " + udoi + "</h1>";
 
-/* call the calcualateTimeUntil() function as a call back function 
-from within a setInterval() function call, passing to it the udoi.*/
-setInterval(calculateTimeUntil, 1000, udoi);
+//create a new timer object
+let myTimer = new Timer(udoi);
 
-function calculateTimeUntil(dateOfInterest){
+setInterval(()=> {
+        myTimer.doTheMath(),
+        daysElement.innerHTML = myTimer.absoluteDaysToGo + ` <br>days`;
+        hoursElement.innerHTML = myTimer.remainderHoursToGo + ` <br>hours`;
+        minutesElement.innerHTML = myTimer.remainderMinutesToGo + ` <br>minutes`;
+        secondsElement.innerHTML = myTimer.remainderSecondsToGo + ` <br>seconds`;
+    }, 100);
+
+function Timer(dateOfInterest){
     /*The calculateTimeUntil() function takes a single argument, namely
     the dateOfInterest. This is a date object that the user has passed 
-    that is... the date of interest! The function subtracts todays date 
-    from the udoi, and then proceeds to split out days, hours, minutes 
-    and time*/
-    
-    let doi = new Date(dateOfInterest).getTime();
-    let todaysDate = new Date().getTime();
-
-    let difference = new Date(doi - todaysDate);
-    let differencems = parseInt(difference.getTime().toString())
+    that is... the date of interest!*/
+    this.doi = new Date(dateOfInterest).getTime();
     
     const completeDayInms = 24*60*60*1000;
     const completeHourInms = 60*60*1000;
     const completeMinInms = 60*1000;
     const completeSecInms = 1000;
 
-    let absoluteDaysToGo;
-    let absoluteHoursToGo;
-    let absoluteMinutesToGo;
-    let absoluteSecondsToGo;
+    this.doTheMath = function(){
+        let todaysDate = new Date().getTime();
+        let difference = new Date(this.doi - todaysDate);
+        let differencems = parseInt(difference.getTime().toString());
 
-    
-    absoluteDaysToGo = Math.floor(differencems/completeDayInms)
-    absoluteHoursToGo = Math.floor(differencems/completeHourInms);
-    absoluteMinutesToGo = Math.floor(differencems/completeMinInms);
-    absoluteSecondsToGo = Math.floor(differencems/completeSecInms);
+        this.absoluteDaysToGo = Math.floor(differencems/completeDayInms)
+        this.absoluteHoursToGo = Math.floor(differencems/completeHourInms);
+        this.absoluteMinutesToGo = Math.floor(differencems/completeMinInms);
+        this.absoluteSecondsToGo = Math.floor(differencems/completeSecInms);
 
-    if (absoluteDaysToGo > 0){
-        remainderHoursToGo = absoluteHoursToGo % (absoluteDaysToGo*24);
-    } else {
-        remainderHoursToGo = absoluteHoursToGo;
+        if (this.absoluteDaysToGo > 0){
+            this.remainderHoursToGo = this.absoluteHoursToGo % (this.absoluteDaysToGo*24);
+        } else {
+            this.remainderHoursToGo = this.absoluteHoursToGo;
+        }
+
+        if (this.absoluteHoursToGo > 0){
+            this.remainderMinutesToGo = this.absoluteMinutesToGo % (this.absoluteHoursToGo*60);
+        } else {
+            this.remainderMinutesToGo = this.absoluteMinutesToGo;
+        }
+
+        if (this.absoluteMinutesToGo > 0){
+            this.remainderSecondsToGo = this.absoluteSecondsToGo % (this.absoluteMinutesToGo*60);
+        } else {
+            this.remainderSecondsToGo = this.absoluteSecondsToGo;
+        }
     }
-
-    if (absoluteHoursToGo > 0){
-        remainderMinutesToGo = absoluteMinutesToGo % (absoluteHoursToGo*60);
-    } else {
-        remainderMinutesToGo = absoluteMinutesToGo;
-    }
-
-    if (absoluteMinutesToGo > 0){
-        remainderSecondsToGo = absoluteSecondsToGo % (absoluteMinutesToGo*60);
-    } else {
-        remainderSecondsToGo = absoluteSecondsToGo;
-    }
-    
-    
-    // Display the timer on the screen
-    daysElement.innerHTML = absoluteDaysToGo + ` <br>days`;
-    hoursElement.innerHTML = remainderHoursToGo + ` <br>hours`;
-    minutesElement.innerHTML = remainderMinutesToGo + ` <br>minutes`;
-    secondsElement.innerHTML = remainderSecondsToGo + ` <br>seconds`;
 }
